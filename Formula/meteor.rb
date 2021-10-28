a9807e9012234ddc97a7e94edf426b1492d3f8fe
Ravi Suhag <suhag.ravi@gmail.com>
2021-10-27 23:14:05 -0700

    Brew formula update for meteor version v0.1.12

--- Formula/meteor.rb
+++ Formula/meteor.rb
@@ -5,39 +5,54 @@
 class Meteor < Formula
   desc "Metadata collection tool."
   homepage "https://github.com/odpf/meteor"
-  version "0.1.11"
+  version "0.1.12"
   license "Apache 2.0"
-  bottle :unneeded
 
   on_macos do
-    if Hardware::CPU.arm?
-      url "https://github.com/odpf/meteor/releases/download/v0.1.11/meteor_0.1.11_macos_arm64.tar.gz"
-      sha256 "72d79c68ffd3b54fab3eb78bb1af06f322b4ef72460854583fd0587b7f922df9"
-    end
     if Hardware::CPU.intel?
-      url "https://github.com/odpf/meteor/releases/download/v0.1.11/meteor_0.1.11_macos_x86_64.tar.gz"
-      sha256 "261808733c60bb4bdcb5fd3b9bcbd63f6c843826546794b07faec3a97c3060f5"
+      url "https://github.com/odpf/meteor/releases/download/v0.1.12/meteor_0.1.12_macos_x86_64.tar.gz"
+      sha256 "b524181604cfccce85ea5c6467b44605fff04fa51f8f0617ce99fdeedb28593f"
+
+      def install
+        bin.install "meteor"
+      end
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/odpf/meteor/releases/download/v0.1.12/meteor_0.1.12_macos_arm64.tar.gz"
+      sha256 "4dd88b20b8d936710ec349501101606dbab51976b0cf16f0b16f1476c2772ca0"
+
+      def install
+        bin.install "meteor"
+      end
     end
   end
 
   on_linux do
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/odpf/meteor/releases/download/v0.1.11/meteor_0.1.11_linux_arm64.tar.gz"
-      sha256 "39ace89ca42a4fc932be25234d200b893eb02c01745adb65d38a5c63bb0797c6"
+      url "https://github.com/odpf/meteor/releases/download/v0.1.12/meteor_0.1.12_linux_arm64.tar.gz"
+      sha256 "13d7818d0e22900b0963c7e99fe27fe289cfb87c5d99d8b3beaba788620041a3"
+
+      def install
+        bin.install "meteor"
+      end
     end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/odpf/meteor/releases/download/v0.1.11/meteor_0.1.11_linux_armv6.tar.gz"
-      sha256 "c162e7cfea06a38d505a63ec12a622ce1a5984b005c12319ef3bbc59923a67df"
+      url "https://github.com/odpf/meteor/releases/download/v0.1.12/meteor_0.1.12_linux_armv6.tar.gz"
+      sha256 "98f7058c8c66cee28246625aa9c18418215daa4a483fa09bca2eddaf674e3966"
+
+      def install
+        bin.install "meteor"
+      end
     end
     if Hardware::CPU.intel?
-      url "https://github.com/odpf/meteor/releases/download/v0.1.11/meteor_0.1.11_linux_x86_64.tar.gz"
-      sha256 "f4f28821412b97e7e9ce5e16229486123466d24c06e24ff7695ab7cc063caf8d"
+      url "https://github.com/odpf/meteor/releases/download/v0.1.12/meteor_0.1.12_linux_x86_64.tar.gz"
+      sha256 "168a06404a570c1d8127ee4fac62d7f08166547a3ada4d837e01bb1c7073bd26"
+
+      def install
+        bin.install "meteor"
+      end
     end
   end
 
   depends_on "git"
-
-  def install
-    bin.install "meteor"
-  end
 end
