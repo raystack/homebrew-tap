f2c64596992a9fa4dc5bf75d36928b6ff1e26ed4
Ravi Suhag <suhag.ravi@gmail.com>
2022-07-19 06:28:43 -0500

    Brew formula update for siren version v0.4.1

--- Formula/siren.rb
+++ Formula/siren.rb
@@ -5,12 +5,12 @@
 class Siren < Formula
   desc "Universal data observability tool."
   homepage "https://github.com/odpf/siren"
-  version "0.4.0"
+  version "0.4.1"
   license "Apache 2.0"
 
   on_macos do
-    url "https://github.com/odpf/siren/releases/download/v0.4.0/siren_0.4.0_macos_x86_64.tar.gz"
-    sha256 "64b9bcfb7d1e7c12400f6ff0f8e770b977b964dd7df42165f301a6b248db2738"
+    url "https://github.com/odpf/siren/releases/download/v0.4.1/siren_0.4.1_macos_x86_64.tar.gz"
+    sha256 "39e0f467d6f6c9ed9d8f04a1fd19ebd0c7764307b31287fb59332b4697f6538c"
 
     def install
       bin.install "siren"
@@ -29,8 +29,8 @@ class Siren < Formula
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/odpf/siren/releases/download/v0.4.0/siren_0.4.0_linux_x86_64.tar.gz"
-      sha256 "25128f93ff3e27fc050e3984ad9e1b427333d82a903429493a1bb0f673050be2"
+      url "https://github.com/odpf/siren/releases/download/v0.4.1/siren_0.4.1_linux_x86_64.tar.gz"
+      sha256 "65dc2148a2eb16298487ad0eebc088ff9f6b8ba5fd5ec7f97277ff85187bae0e"
 
       def install
         bin.install "siren"
